f2196b89cbd0c486de1581b26132b2a2fa9346e3
Ghislaine de Holanda <s1153521@student.windesheim.nl>
2020-11-23 22:30:15 +0100

    daily standuppagina aangepast

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\Windesheim ADSD20\Periode 2\scrumapp\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\Windesheim ADSD20\Periode 2\scrumapp\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Users\Ghislaine\code\scrumapp1\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Ghislaine\code\scrumapp1\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
