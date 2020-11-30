cb537a2aa597b59754a916a52143af934a65628c
amjad-alarori <my@self@1996>
2020-11-30 12:00:24 +0100

    product backlog controller aangepast

---
+++ app/Http/Controllers/ProductBacklogController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ProductBacklog;
+use Illuminate\Http\Request;
+
+class ProductBacklogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ProductBacklog  $productBacklog
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ProductBacklog $productBacklog)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ProductBacklog  $productBacklog
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ProductBacklog $productBacklog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ProductBacklog  $productBacklog
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ProductBacklog $productBacklog)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ProductBacklog  $productBacklog
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ProductBacklog $productBacklog)
+    {
+        //
+    }
+}
