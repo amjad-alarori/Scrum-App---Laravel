03b7100c88a88d393e3cb8928d5fbd338684ac8f
houwaidaelamrani <s1162876@student.windesheim.nl>
2020-12-01 16:10:10 +0100

    to update to latest pull

---
+++ resources/views/vendor/jetstream/components/algemeendashboard.blade.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width", initial-scale="1.0">
+<title>Dashboard Algemeen</title>
+<link rel="stylesheet" href="https://use.fontawesome.com/release/v5.6.3/css/all.css">
+<link rel="stylesheet" href="CSS/styling.css">
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.3/dist/alpine.js" defer></script>
+</head>
+
+<body>
+<section>
+    <div class="container">
+        <h1 class="heading">DASHBOARD ALGEMEEN</h1>
+        <div class="card-wrapper">
+            <div class="card">
+                <img src="images/coding.jpeg" alt="card background" class="card-img">
+                <h1>Profiel</h1>
+                <p class="about">There are many variations of passages of Lorem Ipsum available, but the majority have
+                    suffered alteration in some form, by injected humour, or randomised words which don't look
+                    even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure
+                    there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on
+                    the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+                <a href="#" class="btn">access</a>
+            </div>
+            <div class="card">
+                <img src="images/coding.jpeg" alt="card background" class="card-img">
+                <h1>Projecten</h1>
+                <p class="about">There are many variations of passages of Lorem Ipsum available, but the majority have
+                    suffered alteration in some form, by injected humour, or randomised words which don't look
+                    even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure
+                    there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on
+                    the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+                <a href="#" class="btn">access</a>
+            </div>
+            <div class="card">
+                <img src="images/coding.jpeg" alt="card background" class="card-img">
+                <h1>Teams</h1>
+                <p class="about">There are many variations of passages of Lorem Ipsum available, but the majority have
+                    suffered alteration in some form, by injected humour, or randomised words which don't look
+                    even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure
+                    there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on
+                    the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+                <a href="#" class="btn">access</a>
+            </div>
+            <div class="card">
+                <img src="images/coding.jpeg" alt="card background" class="card-img">
+                <h1>New Project</h1>
+                <p class="about">There are many variations of passages of Lorem Ipsum available, but the majority have
+                    suffered alteration in some form, by injected humour, or randomised words which don't look
+                    even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure
+                    there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on
+                    the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
+                <a href="#" class="btn">access</a>
+            </div>
+        </div>
+    </div>
+</section>
+
+</body>
