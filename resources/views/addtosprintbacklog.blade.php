33df6146d4c0be1d370c567c8f1e691ca9174f90
Roderik Schiele <rorospits@hotmail.com>
2020-12-06 17:16:07 +0100

    sprint dashboard map deleted

---
+++ resources/views/addtosprintbacklog.blade.php
@@ -0,0 +1,88 @@
+@extends('layouts.layout')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+
+
+    <!-- Header -->
+    <header class="bg-primary py-5 mb-5">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-lg-12">
+
+
+
+                    <h1 class="display-4 text-white mt-5 mb-2"> Add productbacklog items to sprint: {{$sprint->title}}</h1>
+
+
+                </div>
+            </div>
+        </div>
+    </header>
+
+
+
+    <div class="container">
+
+
+
+
+        <div class="rowRetrospective">
+
+
+
+
+            <div class="columnRetrospective" id="retrospectiveGood">
+                <h3 class="h3 columnRetrospectiveTitle">What went Good? </h3>
+                <br>
+
+
+                @foreach($retrospective as $comment)
+                    @if ($comment->category==1)
+
+                <div class="input-group overflow">
+                    <div class="cardRetrospective">
+                        <p class="card-text">{{$comment->text}}</p>
+                    </div>
+                    <div class="card-footer text-muted">
+                       Written by {{$comment->user->name}} on {{date("d/m/Y",strtotime($comment->created_at))}}
+                    </div>
+                </div>
+                    @endif
+                    @endforeach
+
+            </div>
+
+            <div class="columnRetrospective" id="retrospectiveImprove">
+             <h3 class="h3 columnRetrospectiveTitle">Which things can be improved?</h3>
+                <br>
+
+             @foreach ($retrospective as $comment)
+             @if($comment->category==2)
+
+                 <div class="input-group overflow">
+                    <div class="cardRetrospective">
+                      <p class="card-text">{{$comment->text}}</p>
+                    </div>
+                    <div class="card-footer text-muted">
+                    Written by {{$comment->user->name}} on {{date("d/m/Y",strtotime($comment->created_at))}}
+                    </div>
+                 </div>
+                 @endif
+                 @endforeach
+
+            </div>
+
+
+
+
+        </div>
+
+
+        <!-- /.row -->
+    </div>
+        <!-- /.container -->
+@endsection
