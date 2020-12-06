9257d20be257bca513811ded7ce09964c326cae1
Ghislaine de Holanda <s1153521@student.windesheim.nl>
2020-12-06 03:00:23 +0100

    aanpassing review en dailystandup-pagina en routes

--- app/Models/Sprint.php
+++ app/Models/Sprint.php
@@ -22,4 +22,9 @@ class Sprint extends Model
     {
         return $this->hasMany(Retrospective::class, 'sprint_id', 'id');
     }
+
+    public function dailyStandUp()
+    {
+        return $this->hasMany(DailyStandUp::class, 'sprint_id', 'id');
+    }
 }
