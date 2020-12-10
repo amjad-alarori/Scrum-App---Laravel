e70ba0d66ae72497d543d7e73719216eea3007ff
Roderik Schiele <rorospits@hotmail.com>
2020-12-10 13:23:46 +0100

    Copied Scrumboard and CSS to master

--- resources/views/scrumBoard.blade.php
+++ resources/views/scrumBoard.blade.php
@@ -9,145 +9,127 @@
 
 
     <!-- Header -->
-    <header class="bg-primary py-5 mb-5">
-        <div class="container h-100">
-            <div class="row h-100 align-items-center">
-                <div class="col-lg-12">
-                    <h1 class="display-4 text-white mt-5 mb-2">Scrum Board</h1>
+    {{--    <header class="bg-primary py-5 mb-5">--}}
+    {{--        <div class="container h-100">--}}
+    {{--            <div class="row h-100 align-items-center">--}}
+    {{--                <div class="col-lg-12">--}}
+    {{--                    <h1 class="display-4 text-white mt-5 mb-2">Scrum Board</h1>--}}
 
-                </div>
-            </div>
-        </div>
-    </header>
+    {{--                </div>--}}
+    {{--            </div>--}}
+    {{--        </div>--}}
+    {{--    </header>--}}
+
+
 
     <!-- Page Content -->
-    <div class="container">
-
-        <div class="row">
-            <div class="col-md-8 mb-5">
-                <h2>Scrum Boardinformatie</h2>
+    <div class="row equal">
+        <div class="col-md-4 col-sm-6">
+            <div class="column-scrumBoard">
+                <h4 class="h4">To Do</h4><br>
                 <hr>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A deserunt neque tempore recusandae animi soluta quasi? Asperiores rem dolore eaque vel, porro, soluta unde debitis aliquam laboriosam. Repellat explicabo, maiores!</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis optio neque consectetur consequatur magni in nisi, natus beatae quidem quam odit commodi ducimus totam eum, alias, adipisci nesciunt voluptate. Voluptatum.</p>
-                <a class="btn btn-primary" href="#">Meer informatie over Scrum Board</a>
+                <br>
+
+                <div class="input-group overflow">
+                    <h5 class="h5ScrumBoard">Backlog Item</h5>
+                    <hr class="lineBacklogItem">
+                    <br>
+
+                    <div id="accordion">
+                        <div class="cardBacklogItem">
+
+                            <button class="btnScrumBoard btn-linkScrumBoard text-decoration-none" data-toggle="collapse" data-target="#collapse" aria-expanded="false" aria-controls="collapse">
+                                More info <span class="arrow">&#8681;</span>
+                            </button>
+
+
+                            <div id="collapse" class="collapse" aria-labelledby="heading">
+                                <div class="card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="margin-top-10">
+                        <button class="buttonScrumBoard button-backlogScrumBoard">To Do</button><button class="buttonScrumBoard button-progressScrumBoard">In Progress</button><button class="buttonScrumBoard button-doneScrumBoard">Done</button><button class="buttonScrumBoard button-deleteScrumBoard">Delete</button>
+                    </div>
+                </div>
+
+
+                <div class="input-group overflow">
+                    <h5 class="h5ScrumBoard">Backlog Item</h5>
+                    <hr class="lineBacklogItem">
+                    <br>
+
+
+                    <div id="accordion">
+                        <div class="cardBacklogItem">
+
+                            <button class="btnScrumBoard btn-linkScrumBoard text-decoration-none" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                More info <span class="arrow">&#8681;</span>
+                            </button>
+
+
+                            <div id="collapseThree" class="collapse" aria-labelledby="headingThree">
+                                <div class="card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <br>
+
+                    <div class="margin-top-10">
+                        <button class="buttonScrumBoard button-backlogScrumBoard">To Do</button><button class="buttonScrumBoard button-progressScrumBoard">In Progress</button><button class="buttonScrumBoard button-doneScrumBoard">Done</button><button class="buttonScrumBoard button-deleteScrumBoard">Delete</button>
+                    </div>
+                </div>
             </div>
-            <div class="col-md-4 mb-5">
-                <h2>Teamleden</h2>
+        </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="column-scrumBoard">
+                <h4 class="h4">In Progress</h4><br>
                 <hr>
-                <address>
-                    <br> Naam 1
-                    <br> Naam 2
-                    <br> Naam 3
-                    <br> Naam 4
-                    <br> Naam 5
-                    <br> Naam 6
-                </address>
+                <br>
+                <div class="input-group overflow">
+                    <h5 class="h5ScrumBoard">Backlog Item</h5>
+                    <hr class="lineBacklogItem">
+                    <br>
 
-            </div>
-        </div>
-        <!-- /.row -->
 
-        <div class="row">
-            <div class="col-md-3 mb-5">
-                <div class="card h-100">
-                    <img class="card-img-top" src="{{ asset('images/sprint toevoegen.jpg') }}" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Sprint toevoegen</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="#" class="btn btn-primary">Voeg een sprint toe</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 mb-5">
-                <div class="card h-100">
-                    <img class="card-img-top" src="{{ asset('images/scrumteam.jpg') }}" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Team samenstellen</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus totam ut praesentium aut.</p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="#" class="btn btn-primary">Stel het scrum team samen</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 mb-5">
-                <div class="card h-100">
-                    <img class="card-img-top" src="{{ asset('images/dod.jpg') }}" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Definition of Done</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.</p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="#" class="btn btn-primary">Bekijken</a>
-                    </div>
-                </div>
-            </div>
+                    <div id="accordion">
+                        <div class="cardBacklogItem">
 
-            <div class="col-md-3 mb-5">
-                <div class="card h-100">
-                    <img class="card-img-top" src="{{ asset('images/productbacklog.jpg') }}" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Product Backlog</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
+                            <button class="btnScrumBoard btn-linkScrumBoard text-decoration-none" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                More info <span class="arrow">&#8681;</span>
+                            </button>
+
+
+                            <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo">
+                                <div class="card-body">
+                                    Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div class="card-footer">
-                        <a href="#" class="btn btn-primary">Bekijken</a>
+                    <br>
+
+                    <div class="margin-top-10">
+                        <button class="buttonScrumBoard button-backlogScrumBoard">To Do</button><button class="buttonScrumBoard button-progressScrumBoard">In Progress</button><button class="buttonScrumBoard button-doneScrumBoard">Done</button><button class="buttonScrumBoard button-deleteScrumBoard">Delete</button>
                     </div>
                 </div>
             </div>
         </div>
+        <div class="col-md-4 col-sm-6">
+            <div class="column-scrumBoard">
+                <h4 class="h4">Done</h4><br>
+                <hr>
 
-
-            <h1 class="display-4 mt-5 mb-2">Sprints</h1>
-            <hr>
-            <br>
-            <br>
-
-        <div class="row">
-            <div class="col-md-4 mb-5">
-                <div class="card h-100">
-                    <img class="card-img-top" src="https://placehold.it/300x200" alt="">
-                    <div class="card-body">
-                        <h4 class="card-title">Sprint 1</h4>
-                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-                    </div>
-                    <div class="card-footer">
-                        <a href="#" class="btn btn-primary">Ga naar sprint</a>
-                    </div>
-                </div>
-            </div>
-
-        <div class="col-md-4 mb-5">
-            <div class="card h-100">
-                <img class="card-img-top" src="https://placehold.it/300x200" alt="">
-                <div class="card-body">
-                    <h4 class="card-title">Sprint 2</h4>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-                </div>
-                <div class="card-footer">
-                    <a href="#" class="btn btn-primary">Ga naar sprint</a>
-                </div>
             </div>
         </div>
-
-        <div class="col-md-4 mb-5">
-            <div class="card h-100">
-                <img class="card-img-top" src="https://placehold.it/300x200" alt="">
-                <div class="card-body">
-                    <h4 class="card-title">Sprint 3</h4>
-                    <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque sequi doloribus.</p>
-                </div>
-                <div class="card-footer">
-                    <a href="#" class="btn btn-primary">Ga naar sprint</a>
-                </div>
-            </div>
-        </div>
-
     </div>
-        <!-- /.row -->
 
-    </div>
-    <!-- /.container -->
+
+
 @endsection
