0c5546687b629010ad241e28bb81ea034db2e8cd
Ali Saviz <ali.saviz@gmail.com>
2020-12-14 13:24:49 +0100

    Seeder changed with loop

--- database/seeders/ScrumTeamSeeder.php
+++ database/seeders/ScrumTeamSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\Project;
 use App\Models\ScrumTeam;
 use App\Models\User;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 
 class ScrumTeamSeeder extends Seeder
@@ -16,11 +17,36 @@ class ScrumTeamSeeder extends Seeder
      */
     public function run()
     {
-        $count = Project::all()->count();
-//        $users = User::query()->orderBy('id')->limit($count)->pluck('id');
+        $factory = new Factory();
+        $faker = $factory->create();
 
-        ScrumTeam::factory()->count($count)->create(['roleId'=>1]);
-        ScrumTeam::factory()->count($count)->create(['roleId'=>2]);
-        ScrumTeam::factory()->count($count*3)->create(['roleId'=>3]);
+        $projects = Project::all();
+
+        $users = User::query()->orderBy('id')->limit(count($projects))->pluck('id')->toArray();
+        foreach ($projects as $project):
+            ScrumTeam::factory()->create([
+                'roleId' => 1,
+                'userId' => $faker->randomElement($users),
+                'projectId'=>$project->id
+            ]);
+        endforeach;
+
+        $users = User::query()->orderBy('id')->offset(count($projects))->limit(count($projects))->pluck('id')->toArray();
+        foreach ($projects as $project):
+            ScrumTeam::factory()->create([
+                'roleId' => 2,
+                'userId' => $faker->randomElement($users),
+                'projectId'=>$project->id
+            ]);
+        endforeach;
+
+        $users = User::query()->orderBy('id')->offset(count($projects))->pluck('id')->toArray();
+        foreach ($projects as $project):
+            ScrumTeam::factory()->count(3)->create([
+                'roleId' => 3,
+                'userId' => $faker->randomElement($users),
+                'projectId'=>$project->id
+            ]);
+        endforeach;
     }
 }
